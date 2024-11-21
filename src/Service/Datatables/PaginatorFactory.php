c221b5c48d5fd917747d306a36a2075c0578ef4a
David Renard <david.renard.21@free.fr>
2024-11-21 13:59:39 +0100

    fix: Misconfiguration for PaginatorFactory when using the bundle

--- src/Service/Datatables/PaginatorFactory.php
+++ src/Service/Datatables/PaginatorFactory.php
@@ -9,33 +9,15 @@ class PaginatorFactory
     public const string PAGINATOR_KNP = 'knp';
     public const string PAGINATOR_CUSTOM = 'custom';
 
-    public function __construct(
-        private readonly ContainerInterface $container,
-        private string $mode = self::PAGINATOR_CUSTOM,
-    ) {
-    }
-
-    public function setMode(string $mode): void
+    public function __construct(private readonly ContainerInterface $container)
     {
-        $this->mode = $mode;
     }
 
     public function createPaginator(?string $mode = null): PaginatorInterface
     {
-        if (in_array($mode, [self::PAGINATOR_KNP, self::PAGINATOR_CUSTOM], true)) {
-            $this->mode = $mode;
-        }
-
-        $paginator = match ($this->mode) {
+        return match ($mode ?? self::PAGINATOR_CUSTOM) {
             self::PAGINATOR_KNP => $this->container->get(KnpPaginatorAdapter::class),
-            self::PAGINATOR_CUSTOM => $this->container->get(CustomPaginator::class),
-            default => null,
-        } ?? new CustomPaginator();
-
-        if (!$paginator instanceof PaginatorInterface) {
-            $paginator = new CustomPaginator();
-        }
-
-        return $paginator;
+            default => new CustomPaginator(),
+        };
     }
 }
